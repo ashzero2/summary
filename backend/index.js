849b09aedb7db0d8ae5644a6fb365c1a20d94db7
rahul-pt6046 <rahul-pt6046@rahul-pt6046.tsi.zohocorpin.com>
2023-03-07 14:21:18 +0530

    bruh

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 
 const app = express();
 const port = 3000;
-let UPLOAD_URL;
+let UPLOAD_URL = "default";
 
 const assembly = axios.create({
   baseURL: "https://api.assemblyai.com/v2",
@@ -32,29 +32,39 @@ const getEntity = () => {
       auto_highlights: true,
       entity_detection: true,
     })
-    .then((res) => console.log(res.data))
+    .then((res) => {
+      return res.data;
+    })
     .catch((err) => console.error(err));
 };
+
+async function upFile() {
+  const file = "audio.mp4";
+
+  async function newFile() {
+    fs.readFile(file, async (err, data) => {
+      if (err) return console.error(err);
+
+      await assembly
+        .post("/upload", data)
+        .then((res) => {
+          UPLOAD_URL = res.data.upload_url;
+          console.log(UPLOAD_URL);
+        })
+        .catch((err) => console.error(err));
+    });
+  }
+
+  newFile().then((resp) => console.log(resp));
+  return UPLOAD_URL;
+}
 
 app.get("/:url", async (req, res) => {
   const link = `https://youtu.be/${req.params.url}`;
   ytdl(link, { filter: "audioonly" }).pipe(fs.createWriteStream("audio.mp4"));
 
-  await wait(2000);
-
-  const file = "audio.mp4";
-  console.log("Uploading File");
-  fs.readFile(file, (err, data) => {
-    if (err) return console.error(err);
-
-    assembly
-      .post("/upload", data)
-      .then((res) => {
-        UPLOAD_URL = res.data.upload_url;
-        getEntity();
-      })
-      .catch((err) => console.error(err));
-  });
+  const se = await upFile();
+  console.log(se);
 });
 
 app.listen(port, () => {
