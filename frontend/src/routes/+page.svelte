8919b7237a028af7b131b999fb35ff61a6d4018c
rahul-pt6046 <rahul-pt6046@rahul-pt6046.tsi.zohocorpin.com>
2023-03-07 16:52:02 +0530

    svelte

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+	let text;
+
+	onMount(async () => {
+		const resp = await fetch(
+			'https://api.assemblyai.com/v2/transcript/rn2odr77ne-35a3-4a3b-9472-3248839bb4cf',
+			{
+				method: 'GET',
+				headers: {
+					authorization: '12cdf25c65754487b70db8abce0dea13'
+				}
+			}
+		);
+
+		resp.json().then((data) => {
+			text = JSON.stringify(data);
+		});
+	});
+</script>
+
+<main>
+	<h1>{text}</h1>
+</main>
