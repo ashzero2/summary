e9d6effab0c3da94c9dcf2131a9b6824ab9b17d1
ashzero2 <aswinzero2@gmail.com>
2023-03-08 16:14:29 +0530

    Api inbuild

---
+++ frontend/src/routes/api/download/+server.js
@@ -0,0 +1,22 @@
+import { error } from '@sveltejs/kit';
+import fs from 'fs';
+
+/** @type {import('./$types').RequestHandler} */
+export function GET({ url }) {
+	const min = Number(url.searchParams.get('min') ?? '0');
+	const max = Number(url.searchParams.get('max') ?? '1');
+
+	const d = max - min;
+
+	if (isNaN(d) || d < 0) {
+		throw error(400, 'min and max must be numbers, and min must be less than max');
+	}
+
+	fs.writeFile('link.txt', 'hello', (err) => {
+		if (err) throw err;
+	});
+
+	const random = min + Math.random() * d;
+
+	return new Response(String(random));
+}
